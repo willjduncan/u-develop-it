3ea34b9fb2823c489e70aa207e936e89bdd1a03d
Will Duncan <willduncan@MBP-8323.attlocal.net>
2022-04-11 16:37:01 -0400

    add basic server functionality

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mysql = require('mysql2');
+const PORT = process.env.PORT || 3001;
+const app = express();
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: '',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
